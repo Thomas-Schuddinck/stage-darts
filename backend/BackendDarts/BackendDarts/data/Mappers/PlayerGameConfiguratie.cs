81947b2159ede3ebe05b74d1361cb8127671c756
Thomas Schuddinck <thomas.schuddinck.y3378@student.hogent.be>
2020-02-18 11:46:17 +0100

    test

---
+++ backend/BackendDarts/BackendDarts/data/Mappers/PlayerGameConfiguratie.cs
@@ -0,0 +1,21 @@
+﻿using BackendDarts.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendDarts.data.Mappers
+{
+    public class PlayerGameConfiguratie : IEntityTypeConfiguration<PlayerGame>
+    {
+        public void Configure(EntityTypeBuilder<PlayerGame> builder)
+        {
+            builder.ToTable("PlayerGame");
+            builder.HasKey(pg => pg.PlayerGameId);
+            builder.HasOne(pg => pg.Player).WithMany(p => p.PlayerGames).HasForeignKey(p => p.PlayerId).OnDelete(DeleteBehavior.Restrict);
+            builder.HasOne(pg => pg.Game).WithMany().HasForeignKey(g => g.GameId).OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
