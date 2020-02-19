73df4282c8bb546e1f8df6527c8685d346b02141
Wouter Opsommer <Wouter.Opsommer@realdolmen.com>
2020-02-19 13:22:16 +0100

    Personal stats page design

--- frontend/src/containers/PersonalStatsBuilder/PersonalStatsBuilder.tsx
+++ frontend/src/containers/PersonalStatsBuilder/PersonalStatsBuilder.tsx
@@ -1,16 +1,56 @@
 import React, {Component} from 'react';
 import Person from '../../components/PersonalStats/Person'
 import Aux from '../../hoc/Wrap';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 
-class PersonalStatsBuilder extends Component {
-    render () {
+const useStyles = makeStyles(theme => ({
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+      },
+      fixedHeight: {
+        height: 240,
+      },
+}));
+
+
+
+    export default function PersonalStatsBuilder () {
+
+        const classes = useStyles();
+
+
+        const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+
+
         return (
             <Aux>
+                <Grid container spacing={3}>
+            {/* Chart */}
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper className={fixedHeightPaper}>
                 <Person/>
-                <div>Heatmap?</div>
+              </Paper>
+            </Grid>
+            {/* Recent Deposits */}
+            <Grid item xs={12} md={6} lg={6}>
+              <Paper className={fixedHeightPaper}>
+                <Person/>
+              </Paper>
+            </Grid>
+            {/* Recent Orders */}
+            <Grid item xs={12}>
+              <Paper className={classes.paper}>
+                <p>history?</p>
+                <p>heatmap?</p>
+              </Paper>
+            </Grid>
+          </Grid>
             </Aux>
         );
     }
-}
-
-export default PersonalStatsBuilder;
