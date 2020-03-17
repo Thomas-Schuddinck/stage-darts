2b14089ea645712c9d9e4791c536c8d80c8d5ede
wouteropsommer <wouter.opsommer@student.hogent.be>
2020-03-17 12:57:45 +0100

    keyboard works with d and t

--- frontend/src/components/Game/AddThrow/AddThrow.tsx
+++ frontend/src/components/Game/AddThrow/AddThrow.tsx
@@ -3,6 +3,7 @@ import { Typography, makeStyles, Grid, Button } from '@material-ui/core';
 import { indigo } from '@material-ui/core/colors';
 import Wrap from '../../../hoc/Wrap'
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
+import PostApiCall from '../../../services/ApiClientPost';
 
 const useStyles = makeStyles(theme => ({
     leg: {
@@ -32,6 +33,13 @@ const useStyles = makeStyles(theme => ({
         width: '100%',
         minWidth: '0',
         border: '0.1em solid black',
+    },
+    buttonSelected: {
+        color: 'black',
+        backgroundColor: indigo[200],
+        width: '100%',
+        minWidth: '0',
+        border: '0.1em solid black',
     }
 }));
 
@@ -39,6 +47,8 @@ const AddThrow = (props: any) => {
     const classes = useStyles();
 
     let [size = 0, setSize] = useState<number>();
+    let [tripple = false, setTripple] = useState<boolean>();
+    let [double = false, setDouble] = useState<boolean>();
 
     useEffect(() => {
         window.addEventListener('resize', updateWindowDimensions);
@@ -49,13 +59,33 @@ const AddThrow = (props: any) => {
         setSize(window.innerWidth);
     }
 
+    const PostThrowCall = async (value: string) => {
+        let val = parseInt(value);
+
+        if (double)
+            val = val * 2;
+        if (tripple)
+            val = val * 3;
+
+        setDouble(false);
+        setTripple(false);
+        console.log(val);
+        return await PostApiCall('https://localhost:5000/Game/game', val.toString()).then(resp => {
+            console.log(resp);
+        });
+    }
+
     const createButtons = () => {
         let container = [];
         let buttons = [];
         for (let i = 1; i <= 20; i++) {
             buttons.push(
                 <Grid item xs={2} md={2} lg={2}>
-                    <Button className={classes.button} key={i}>{i}</Button>
+                    <Button
+                        className={classes.button}
+                        key={i}
+                        onClick={() => PostThrowCall(i.toString())}
+                    >{i}</Button>
                 </Grid>
             )
         }
@@ -67,31 +97,49 @@ const AddThrow = (props: any) => {
         return container;
     }
 
+    const toggleDouble = () => {
+        if (double)
+            setDouble(false);
+        else {
+            setTripple(false);
+            setDouble(true);
+        }
+    }
+
+    const toggleTripple = () => {
+        if (tripple)
+            setTripple(false);
+        else {
+            setDouble(false);
+            setTripple(true);
+        }
+    }
+
     return (
         <Wrap>
 
-        {size < 499 ? (<Grid className={classes.wrap} container>
-            {createButtons()}
-            <Grid item xs={2} md={2} lg={2}>
-                <Button className={classes.button}>25</Button>
-            </Grid>
-            <Grid item xs={2} md={2} lg={2}>
-                <Button className={classes.button}>D</Button>
-            </Grid>
-            <Grid item xs={2} md={2} lg={2}>
-                <Button className={classes.button}>T</Button>
-            </Grid>
-            <Grid item xs={2} md={2} lg={2}>
-                <Button className={classes.photo}><AddAPhotoIcon /></Button>
-            </Grid>
-        </Grid>): (null)}
+            {size < 499 ? (<Grid className={classes.wrap} container>
+                {createButtons()}
+                <Grid item xs={2} md={2} lg={2}>
+                    <Button onClick={() => PostThrowCall('25')} className={classes.button}>25</Button>
+                </Grid>
+                <Grid item xs={2} md={2} lg={2}>
+                    <Button onClick={() => toggleDouble()} className={double ? classes.buttonSelected: classes.button}>D</Button>
+                </Grid>
+                <Grid item xs={2} md={2} lg={2}>
+                    <Button onClick={() => toggleTripple()} className={tripple ? classes.buttonSelected: classes.button}>T</Button>
+                </Grid>
+                <Grid item xs={2} md={2} lg={2}>
+                    <Button className={classes.photo}><AddAPhotoIcon /></Button>
+                </Grid>
+            </Grid>) : (null)}
 
         </Wrap>
 
-            
-        
 
-        
+
+
+
     );
 
 };
