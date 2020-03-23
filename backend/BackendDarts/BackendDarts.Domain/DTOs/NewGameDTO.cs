fb1cacc8f988298752131352e0c9d25ecc4428a1
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-03-23 15:54:44 +0100

    added newgame dto

---
+++ backend/BackendDarts/BackendDarts.Domain/DTOs/NewGameDTO.cs
@@ -0,0 +1,15 @@
+﻿using BackendDarts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendDarts.DTOs
+{
+    public class NewGameDTO
+    {
+        public String Name { get; set; }
+        public int Type { get; set; }
+
+        public List<Player> Players { get; set; }
+    }
+}
