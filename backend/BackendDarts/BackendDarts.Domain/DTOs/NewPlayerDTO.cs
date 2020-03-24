86493ae050a12ca9b2d51216f1b0b23a6d910810
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-03-24 16:01:44 +0100

    added newPlayerDTO

---
+++ backend/BackendDarts/BackendDarts.Domain/DTOs/NewPlayerDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendDarts.Domain.DTOs
+{
+    public class NewPlayerDTO
+    {
+
+        public NewPlayerDTO(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+    }
+}
