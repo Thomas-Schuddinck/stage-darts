06549b6000ee5efd206fe76fe69c5b4136811991
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-04-15 12:12:25 +0200

    added tournament classes

---
+++ backend/BackendDarts/BackendDarts.Domain/Models/Tournament.cs
@@ -0,0 +1,27 @@
+﻿using BackendDarts.DTOs;
+using BackendDarts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendDarts.Domain.Models
+{
+    public class Tournament
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public List<Game> Games { get; set; }
+        public List<PlayerTournament> PlayerTournaments { get; set; } = new List<PlayerTournament>();
+        public int Winner { get; set; }
+
+        public Tournament()
+        {
+            Winner = -1;
+        }
+        public Tournament(NewGameDTO newGameDTO) : this()
+        {
+            Name = newGameDTO.Name;
+
+        }
+    }
+}
