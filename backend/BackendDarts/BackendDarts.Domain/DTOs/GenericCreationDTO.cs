5bcc5ebec0f5efed1f63bcf5395f04639175c52f
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-04-21 14:22:40 +0200

    genericCreationDTO added

---
+++ backend/BackendDarts/BackendDarts.Domain/DTOs/GenericCreationDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendDarts.Domain.DTOs
+{
+    public class GenericCreationDTO
+    {
+        public int Id { get; private set; }
+        public object Body { get; private set; }
+        public GenericCreationDTO(int id, object body)
+        {
+            this.Id = id;
+            this.Body = body;
+        }
+        {
+
+        }
+    }
+}
