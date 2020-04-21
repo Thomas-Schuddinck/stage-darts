5d8afc50fc55ba31a705857335629b53a96df14f
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-04-21 11:42:28 +0200

    create tournament setup

--- backend/BackendDarts/BackendDarts.Domain/Models/Tournament.cs
+++ backend/BackendDarts/BackendDarts.Domain/Models/Tournament.cs
@@ -10,9 +10,9 @@ namespace BackendDarts.Domain.Models
     {
         public int Id { get; set; }
         public String Name { get; set; }
-        public List<Game> Games { get; set; }
+        public List<TournamentGame> Games { get; set; }
         public List<PlayerTournament> PlayerTournaments { get; set; } = new List<PlayerTournament>();
-        public int Winner { get; set; }
+        public int Winner { get; private set; }
 
         public Tournament()
         {
@@ -22,6 +22,37 @@ namespace BackendDarts.Domain.Models
         {
             Name = newGameDTO.Name;
 
+        }
+
+        public void SetupTournament(List<Player> players)
+        {
+            for(int i = 1; i < players.Count; i+=2)
+            {
+                Player player1 = players[i - 1];
+                Player player2 = players[i];
+                AddPlayer(player1);
+                AddPlayer(player2);
+                AddGame(new TournamentGame(i, Name, player1, player2));
+            }
+        }
+        private void AddPlayer(Player player)
+        {
+            PlayerTournaments.Add(new PlayerTournament
+            {
+                Player = player,
+                Tournament = this
+            }
+            );
+        }
+
+        private void AddGame(TournamentGame game)
+        {
+            Games.Add(game);
+        }
+
+        private void SetWinner(int idWinner)
+        {
+            Winner = idWinner;
         }
     }
 }
