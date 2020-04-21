86710241a7d962edfeb0cbb550c0e50dafc69b17
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-04-21 11:06:05 +0200

    added tournamentgame

---
+++ backend/BackendDarts/BackendDarts.Domain/Models/TournamentGame.cs
@@ -0,0 +1,18 @@
+﻿using BackendDarts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackendDarts.Domain.Models
+{
+    public class TournamentGame : Game
+    {
+        public int BracketSectorNumber {get; private set; }
+        public TournamentGame(int bracketSectorNr, string name) : base()
+        {
+            BracketSectorNumber = bracketSectorNr;
+            Name = name + " Tournament - " + PlayerGames.Count + "Players";
+            Type = 3;
+        }
+    }
+}
