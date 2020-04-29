c0089b98ae95a8111c0b9747b5c1d25944acd20f
thomas <thomas.schuddinck.y3378@student.hogent.be>
2020-04-29 15:48:20 +0200

    fix includes tournament

--- backend/BackendDarts/BackendDarts/data/Repos/GameRepository.cs
+++ backend/BackendDarts/BackendDarts/data/Repos/GameRepository.cs
@@ -56,6 +56,8 @@ namespace BackendDarts.Repos
                 .Include(p => p.LegGroups).ThenInclude(lg => lg.PlayerLegs).ThenInclude(pl => pl.Turns).ThenInclude(t => t.Throws)
                 .Include(p => p.PlayerGames).ThenInclude(pg => pg.Player)
                 .Include(p => p.CurrentLegGroup).ThenInclude(lg => lg.PlayerLegs).ThenInclude(pl => pl.Turns).ThenInclude(t => t.Throws)
+                .Include(p => p.Tournament).ThenInclude(t => t.PlayerTournaments)
+                .Include(p => p.Tournament).ThenInclude(t=> t.Games).ThenInclude(g => g.PlayerGames)
                 .SingleOrDefault(a => a.Id == id);
         }
 
